b051a54fc20fabcbeda70939ed258a29175e4d5b
hachem tlijani <tlijanihachem4@gmail.com>
2023-03-11 14:30:01 +0100

    portfolio

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+    <a href="https://github.com/Tlija" className="home__social-icon" target="_blank">
+    <i class="uil uil-github-alt"></i>
+    </a>
+
+    <a href="https://instagram.com/tlijanihachem4?igshid=ZDdkNTZiNTM=" className="home__social-icon" target="_blank">
+    <i class="uil uil-instagram-alt"></i>
+    </a>
+
+
+    <a href="https://www.facebook.com/profile.php?id=100009589751275" className="home__social-icon" target="_blank">
+    <i class="uil uil-facebook-f"></i>
+    </a>
+
+</div>
+  )
+}
+
+export default Social
